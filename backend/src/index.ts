744be772f6d2ea998646527142aa04971f8f6c07
Ruslan Taranov <ruslan.taranov.sakh@gmail.com>
2025-02-23 12:11:04 +0000

    init 2

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,10 @@
 import { Elysia } from "elysia";
+import { cors } from '@elysiajs/cors'
 
-const app = new Elysia().get("/", () => "Сука! Час на Hello World потратил").listen(3000);
+const app = new Elysia()
+  .use(cors())
+  .get("/", () => "Потратил несколько часов на то, чтобы понять, что реакт говно и сделал сэмпл с фронтэндом и бэкэндом в докере")
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+
+  const [data, setData] = useState([]);
+
+  axios.get('http://185.179.190.134:3000/').then(resp=>{
+    console.log('resp')
+    console.log(resp.data)
+    setData(resp.data); // Update state with the fetched data
+  })
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Штука крутится
+        </p>
+        <p>
+          {data}
         </p>
         <a
           className="App-link"
